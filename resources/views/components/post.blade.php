119b88c2ee99b96b0b949895e4da07e736f267c9
Andrea Falcon <andrea.falcon@heply.it>
2021-05-11 18:33:12 +0200

    feat: add info author in post

--- resources/views/components/post.blade.php
+++ resources/views/components/post.blade.php
@@ -1,6 +1,6 @@
 <div>
     <h2><a href="/post/{{ $post->slug }}" title="">{{  $post->title }}</a></h2>
-    <p><a href="/categories/{{  $post->category->slug }}" title=""><small>{{ $post->category->name }}</small></a></p>
+    <p>By <a href="" title="">{{ $post->user->name }}</a> in <a href="/categories/{{  $post->category->slug }}" title=""><small>{{ $post->category->name }}</small></a></p>
     <p>{{ $post->subtitle }}</p>
     <div>{!! $post->description !!}</div>
 </div>
--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <h2>{{  $post->title }}</h2>
-    <p><a href="/categories/{{  $post->category->slug }}" title=""><small>{{ $post->category->name }}</small></a></p>
+    <p>By <a href="" title="">{{ $post->user->name }}</a> in <a href="/categories/{{  $post->category->slug }}" title=""><small>{{ $post->category->name }}</small></a></p>
     <p>{{ $post->subtitle }}</p>
     <div>{!! $post->description !!}</div>
     <a href="/" title="">Go back</a>
