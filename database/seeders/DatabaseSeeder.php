dc05ae2baa3a6d2484358c44e638c27ad750ebe4
Andrea Falcon <andrea.falcon@heply.it>
2021-05-11 18:19:12 +0200

    feat: first factory init db

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
+use App\Models\Post;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -11,8 +14,52 @@ class DatabaseSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
-    {
-        // \App\Models\User::factory(10)->create();
+    public function run() {
+        Category::truncate();
+        User::truncate();
+        Post::truncate();
+
+        $user = User::factory()->create();
+
+        $personal = Category::create(
+            [
+                'name' => 'Personal',
+                'slug' => 'personal',
+            ]
+        );
+        $work     = Category::create(
+            [
+                'name' => 'Work',
+                'slug' => 'work',
+            ]
+        );
+        $test     = Category::create(
+            [
+                'name' => 'For test',
+                'slug' => 'for-test',
+            ]
+        );
+
+        Post::create(
+            [
+                'user_id'     => $user->id,
+                'category_id' => $work->id,
+                'title'       => 'My first post',
+                'slug'        => 'my-first-post',
+                'subtitle'    => 'Lorem ipsum dolar sit amet.',
+                'description' => 'Lorem ipsum dolar sit amet. Lorem ipsum dolar sit amet. Lorem ipsum dolar sit amet.',
+            ]
+        );
+
+        Post::create(
+            [
+                'user_id'     => $user->id,
+                'category_id' => $personal->id,
+                'title'       => 'My work post',
+                'slug'        => 'my-work-post',
+                'subtitle'    => 'Lorem ipsum dolar sit amet.',
+                'description' => 'Lorem ipsum dolar sit amet. Lorem ipsum dolar sit amet. Lorem ipsum dolar sit amet.',
+            ]
+        );
     }
 }
