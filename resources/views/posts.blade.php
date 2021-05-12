7e1acee521a04db10f830e08069c715dd1f34f68
Andrea Falcon <andrea.falcon@heply.it>
2021-05-12 18:09:30 +0200

    feat: dynamic list posts in home

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -3,17 +3,18 @@
 @section('content')
     @include('_posts-header')
     <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
-        <x-post-featured-card />
+        @if($posts->count())
+            <x-post-featured-card :post="$posts->first()"/>
 
-        <div class="lg:grid lg:grid-cols-2">
-            <x-post-card />
-            <x-post-card />
-        </div>
-
-        <div class="lg:grid lg:grid-cols-3">
-            <x-post-card />
-            <x-post-card />
-            <x-post-card />
-        </div>
+            @if($posts->count() > 1)
+                <div class="lg:grid lg:grid-cols-6">
+                    @foreach($posts->skip(1) as $post)
+                        <x-post-card :post="$post" class="{{ $loop->iteration < 3 ? 'col-span-3' : 'col-span-2' }}"/>
+                    @endforeach
+                </div>
+            @endif
+        @else
+            <p class="text-center">No posts yet. Please check back later.</p>
+        @endif
     </main>
 @endsection
