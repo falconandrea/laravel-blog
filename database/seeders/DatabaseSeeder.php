159621dfefffcc5a2d2416533df0407377141f81
Andrea Falcon <andrea.falcon@heply.it>
2021-05-12 11:54:32 +0200

    feat: replace seeds database with correct factories classes

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,51 +15,6 @@ class DatabaseSeeder extends Seeder
      * @return void
      */
     public function run() {
-        Category::truncate();
-        User::truncate();
-        Post::truncate();
-
-        $user = User::factory()->create();
-
-        $personal = Category::create(
-            [
-                'name' => 'Personal',
-                'slug' => 'personal',
-            ]
-        );
-        $work     = Category::create(
-            [
-                'name' => 'Work',
-                'slug' => 'work',
-            ]
-        );
-        $test     = Category::create(
-            [
-                'name' => 'For test',
-                'slug' => 'for-test',
-            ]
-        );
-
-        Post::create(
-            [
-                'user_id'     => $user->id,
-                'category_id' => $work->id,
-                'title'       => 'My first post',
-                'slug'        => 'my-first-post',
-                'subtitle'    => 'Lorem ipsum dolar sit amet.',
-                'description' => '<p>Lorem ipsum dolar sit amet. Lorem ipsum dolar sit amet. Lorem ipsum dolar sit amet.</p>',
-            ]
-        );
-
-        Post::create(
-            [
-                'user_id'     => $user->id,
-                'category_id' => $personal->id,
-                'title'       => 'My work post',
-                'slug'        => 'my-work-post',
-                'subtitle'    => 'Lorem ipsum dolar sit amet.',
-                'description' => '<p>Lorem ipsum dolar sit amet. Lorem ipsum dolar sit amet. Lorem ipsum dolar sit amet.</p>',
-            ]
-        );
+        Post::factory()->create();
     }
 }
