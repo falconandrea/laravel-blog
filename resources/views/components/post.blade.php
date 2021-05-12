d2263e401d9b5d1ca956e095084205abfeb7c5c9
Andrea Falcon <andrea.falcon@heply.it>
2021-05-12 17:18:23 +0200

    fix: rename post-card component file

--- resources/views/components/post.blade.php
+++
@@ -1,6 +0,0 @@
-<div>
-    <h2><a href="/post/{{ $post->slug }}" title="">{{  $post->title }}</a></h2>
-    <p>By <a href="/authors/{{ $post->author->username }}" title="">{{ $post->author->name }}</a> in <a href="/categories/{{  $post->category->slug }}" title=""><small>{{ $post->category->name }}</small></a></p>
-    <p>{{ $post->subtitle }}</p>
-    <div>{!! $post->description !!}</div>
-</div>
