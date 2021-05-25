066d50607f70004dd96bbbe2d1916fbecaca287b
Andrea Falcon <andrea.falcon@heply.it>
2021-05-25 17:51:31 +0200

    feat: split dropdown select in components and dynamic it

---
+++ resources/views/components/dropdown.blade.php
@@ -0,0 +1,12 @@
+@props(['trigger', 'slots'])
+
+<div x-data="{ show: false }" @click.away="show = false">
+    {{-- Trigger --}}
+    <div @click="show = !show">
+        {{ $trigger }}
+    </div>
+    {{-- Links --}}
+    <div x-show="show" class="py-2 absolute bg-gray-100 w-full mt-2 rounded-xl z-50" style="display: none">
+        {{ $slots }}
+    </div>
+</div>
