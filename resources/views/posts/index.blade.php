ae96bb5a278cb439be073da0f951d35275cf87f9
Andrea Falcon <andrea.falcon@heply.it>
2021-05-28 18:05:29 +0200

    feat: move posts views in subfolder and rename it

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.base')
 
 @section('content')
-    @include('_posts-header')
+    @include('posts._header')
     <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
         @if($posts->count())
             <x-post-featured-card :post="$posts->first()"/>
