6d9fec6758384ed4099e386c01a3d8054d5f4eaf
Andrea Falcon <923390-andrea.falcon@users.noreply.gitlab.com>
2021-07-13 18:44:02 +0200

    feat: hide form comments for guests

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -52,6 +52,22 @@
                     </div>
                 </div>
                 <section class="col-span-8 col-start-5 mt-10">
+                    @auth
+                        <form method="POST" action="/posts/{{ $post->slug }}/comments" class="border border-gray-200 p-6 rounded-xl mb-4">
+                            @csrf
+                            <header class="flex items-center">
+                                <img src="https://i.pravatar.cc/60?us={{ auth()->id() }}" alt="" width="40" height="40" class="rounded-full" />
+                                <h2 class="ml-4">Want to partecipate?</h2>
+                            </header>
+                            <div class="mt-6">
+                                <textarea name="body" class="w-full text-sm focus:outline-none focus:ring" cols="30" rows="5" placeholder="Write something"></textarea>
+                            </div>
+                            <div class="flex justify-end mt-6 border-t border-gray-200 pt-6">
+                                <button class="bg-blue-500 text-white uppercase font-semibold text-xs py-2 px-10 rounded-2xl hover:bg-blue-600" type="submit">Post</button>
+                            </div>
+                        </form>
+                    @endauth
+
                     @foreach($post->comments as $comment)
                         <x-post-comment :comment="$comment"/>
                     @endforeach
